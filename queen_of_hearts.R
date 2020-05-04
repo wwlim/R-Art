e1122055813dd14bbf5470562fe5b96c9400213a
Wey Wen Lim <limweywen@gmail.com>
2020-05-04 23:41:46 +0800

    Update queen_of_hearts.R

--- queen_of_hearts.R
+++ queen_of_hearts.R
@@ -1,15 +1,11 @@
-# Heart shaped to one's content
-# Goal: Build code to draw a heart shape according to one's wishes :)
-# Current progress + next steps: 
-# 1. Adapted basic plot from: https://stackoverflow.com/questions/8082429/plot-a-heart-in-r by commenter 42-
-# 2. Thinking of ways to be able to make this modifiable
-# 3. Try plotting other formulas?
+# Simple heart
+# Drawing a simple heart shape
 
 # Blank plot
 plot.new()
 
 # Set up t values
-dat <- data.frame(t=seq(0, 2*pi, by=0.1)) 
+dat <- data.frame(t=seq(0, 2*pi, by=0.02)) 
 
 # Function to generate X and Y values to draw a heart
 xhrt <- function(t) 16*sin(t)^3
@@ -19,7 +15,10 @@ yhrt <- function(t) 13*cos(t)-5*cos(2*t)-2*cos(3*t)-cos(4*t)
 dat$y=yhrt(dat$t)
 dat$x=xhrt(dat$t)
 
+# Draw the shape
+color <- "hotpink"
+
 with(dat, plot(x,y, type="l", col="white", axes=F, xlab=NA, ylab=NA))
-with(dat, polygon(x,y, border="white", col="hotpink"))   
+with(dat, polygon(x,y, border="white", col=color))   
 
 # Script end #
